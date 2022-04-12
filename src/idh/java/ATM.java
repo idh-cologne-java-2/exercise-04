e4f229097ec6096ea8b710f9fb1a0082ddc7523c
Nils Reiter <nils.reiter@uni-koeln.de>
2022-04-12 14:27:08 +0200

    base version from exercise 1

---
+++ src/idh/java/ATM.java
@@ -0,0 +1,38 @@
+package idh.java;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ATM {
+	/**
+	 * Main command loop of the ATM Asks the user to enter a number, and passes this
+	 * number to the function cashout(...) which actually does the calculation and
+	 * produces money. If the user enters anything else than an integer number, the
+	 * loop breaks and the program exists
+	 */
+	public void run() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			try {
+				System.out.print("Enter the amount to withdraw: ");
+				int amount = Integer.parseInt(br.readLine());
+				cashout(amount);
+			} catch (Exception e) {
+				break;
+			}
+		}
+	}
+
+	public void cashout(int amount) {
+
+	};
+
+	/**
+	 * Launches the ATM
+	 */
+	public static void main(String[] args) {
+		ATM atm = new ATM();
+		atm.run();
+	};
+
+}
