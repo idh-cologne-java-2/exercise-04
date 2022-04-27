53d025f4e80e008352e08d3427a2f4a56821efaa
JRadermacher <jana.radermacher7@gmx.de>
2022-04-27 17:02:08 +0200

    Neue Klasse AccountIterator hinzugefügt

---
+++ src/main/java/idh/java/AccountIterator.java
@@ -0,0 +1,19 @@
+package idh.java;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class AccountIterator implements Iterator<Account> {
+
+
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Account next() {
+        return null;
+    }
+}
