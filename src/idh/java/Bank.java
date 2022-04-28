4f820943eef7b8da7627215ade294475b1757f3e
sophiejorns <sophiejorns@sophies-macbook-air.fritz.box>
2022-04-28 17:45:07 +0200

    + neue Klasse Bank, die die Konten speichert

---
+++ src/idh/java/Bank.java
@@ -0,0 +1,18 @@
+package idh.java;
+
+import java.util.Iterator;
+
+public class Bank implements Iterable<Account> {
+	//class Bank can store the accounts
+	public void Account (String accountNumber) {
+		Account account = new Account (accountNumber);
+				return;
+	}
+
+	@Override
+	public Iterator<idh.java.Account> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
